4f72ba6730417167c20931316ba4276b785f8fe6
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-03-04 12:51:39 +0600

    login page

--- App/Screen/Login.js
+++ App/Screen/Login.js
@@ -1,20 +1,20 @@
-import React, { useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { StatusBar } from "expo-status-bar";
 import {
-    StyleSheet,
-    Text,
-    View,
-    TextInput,
-    TouchableOpacity,
-    Button,
-    Image,
-  } from "react-native";
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Button,
+  Image,
+} from "react-native";
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { auth } from '../../firebase'
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 
-const Login=(props)=>
-{
+const Login = (props) => {
   const navigation = useNavigation()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -29,7 +29,7 @@ const Login=(props)=>
 
     return unsubscribe
   }, [])
-  
+
 
   const handleLogin = () => {
     auth
@@ -38,101 +38,152 @@ const Login=(props)=>
         const user = userCredentials.user;
         console.log('Logged in with:', user.email);
       })
-       .catch(error => alert(error.message))
+      .catch(error => alert(error.message))
   }
-  
-    return (
-        <View style={styles.container}>
-      
-         
-        <StatusBar style="auto" />
-        <View >
-         <Image style={styles.img}  source={{
+
+  return (
+    <View style={styles.container}>
+
+
+      <StatusBar style="auto" />
+
+      <View >
+        <Image style={styles.img} source={{
           uri: 'https://i.ibb.co/0cnkFmM/profile.png',
-        }}/>
+        }} />
+      </View>
+      <View style={styles.inputView}>
+        <View style={styles.iconStyle}>
+          <AntDesign name={'user'} size={25} color="#666" />
         </View>
-        <View style={styles.inputView}>
-          <TextInput
-            style={styles.TextInput}
-            placeholder="Email."
-            placeholderTextColor="#003f5c"
-            onChangeText={text => setEmail(text)}
-          />
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          onChangeText={text => setEmail(text)}
+          keyboardType="email-address"
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <View style={styles.iconStyle}>
+          <AntDesign name={'lock'} size={25} color="#666" />
         </View>
-  
-        <View style={styles.inputView}>
-          <TextInput
-            style={styles.TextInput}
-            placeholder="Password."
-            placeholderTextColor="#003f5c"
-            secureTextEntry={true}
-             onChangeText={text=>setPassword(text)}
-          />
-        </View>
-        <TouchableOpacity>
+
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={text => setPassword(text)}
+
+          secureTextEntry={true}
+        />
+      </View>
+      <TouchableOpacity>
+
         <Text style={styles.forgot_button}>Forgot Password?</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.loginBtn} >
+        <View style={styles.iconStyle}>
+          <AntDesign name={'login'} size={25} color="#666" />
+        </View>
         <Text style={styles.loginText} onPress={handleLogin}>LOGIN</Text>
       </TouchableOpacity>
+      <TouchableOpacity style={styles.regBtn} >
+        <View style={styles.iconStyle}>
+          <AntDesign name={'form'} size={25} color="#666" />
+        </View>
+        <Text style={styles.loginText} onPress={() => navigation.navigate("Register")}>Register</Text>
+      </TouchableOpacity>
 
-       <TouchableOpacity style={styles.loginBtn} >
-        <Text style={styles.loginText} onPress={() =>navigation.navigate("Register")}>Register</Text>
-      </TouchableOpacity> 
-    
-     </View>
+    </View>
 
-      
-    )
+
+  )
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-  
-  
-    inputView: {
-      backgroundColor: "#FFC0CB",
-      borderRadius: 30,
-      width: "70%",
-      height: 45,
-      marginBottom: 20,
-  
-      alignItems: "center",
-    },
-  
-    TextInput: {
-      height: 50,
-      flex: 1,
-      padding: 10,
-      marginLeft: 20,
-    },
-  
-    forgot_button: {
-      height: 30,
-      marginBottom: 30,
-    },
-  
-    loginBtn: {
-      width: "80%",
-      borderRadius: 25,
-      height: 50,
-      alignItems: "center",
-      justifyContent: "center",
-      marginTop: 40,
-      backgroundColor: "#FF1493",
-    },
-    img:
-    {
-      marginBottom:20,
-      marginStart:20,
-      width:100,
-      height:100
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20
+  },
+
+
+  inputView: {
+    marginTop: 5,
+    marginBottom: 10,
+    width: '100%',
+    borderColor: '#ccc',
+    borderRadius: 3,
+    borderWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+
+  },
+  iconStyle: {
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRightColor: '#ccc',
+    borderRightWidth: 1,
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    textAlign: 'center'
+  },
+
+  forgot_button: {
+    fontSize: 13,
+    fontWeight: '500',
+    color: '#191970',
+    marginBottom: 10,
+    marginLeft: 35
+  },
+
+
+  loginBtn: {
+    marginTop: 5,
+    marginBottom: 10,
+    height: 60,
+    width: '100%',
+    borderColor: '#ccc',
+    backgroundColor: '#f5e7ea',
+    borderRadius: 3,
+    borderWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  regBtn: {
+
+    height: 60,
+    width: '100%',
+    borderColor: '#ccc',
+    backgroundColor: '#e6eaf4',
+    borderRadius: 3,
+    borderWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  loginText: {
+    flex: 1,
+    textAlign: 'center',
+    fontSize: 13,
+  },
+  img:
+  {
+    marginBottom: 20,
+    marginStart: 20,
+    width: 100,
+    height: 100,
+    resizeMode: 'cover'
+  }
+});
 
 export default Login;
 
--- App/Screen/Setting.js
+++ App/Screen/Setting.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View ,Image,TouchableOpacity} from 'react-native'
-import React, { useEffect, useState} from 'react'
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { auth } from '../../firebase'
 import { Divider } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native';
@@ -12,7 +12,7 @@ import { useNavigation } from '@react-navigation/native';
 const Setting = () => {
   const navigation = useNavigation()
 
-   const [Admin,setAdmin]=useState(false)
+  const [Admin, setAdmin] = useState(false)
 
   const handleSignOut = () => {
     auth
@@ -23,133 +23,133 @@ const Setting = () => {
       .catch(error => alert(error.message))
   }
 
-  
-  useEffect(() => {
-    
-    fetch('https://quiet-lowlands-93783.herokuapp.com/member')
-        .then(res=>res.json())
-        .then(data=>{
-           for(var i=0; i<data.length; i++)
-           {
-               if(data[i].email===auth.currentUser?.email){
-               console.log(data[i].role);
-               if(data[i].role){
-                   setAdmin(true)
-                   return;
-               }
-               }
-               else
-               {
-                   setAdmin(false)
-               }
-           }
-        })
-      }, [])
 
-  
-  
+  useEffect(() => {
+
+    fetch('https://quiet-lowlands-93783.herokuapp.com/member')
+      .then(res => res.json())
+      .then(data => {
+        for (var i = 0; i < data.length; i++) {
+          if (data[i].email === auth.currentUser?.email) {
+            console.log(data[i].role);
+            if (data[i].role) {
+              setAdmin(true)
+              return;
+            }
+          }
+          else {
+            setAdmin(false)
+          }
+        }
+      })
+  }, [])
+
+
+
 
   return (
-    <View>
-      {/* div brefore first horizental break */}
-       <View style={styles.container}>
-         <Image style={styles.setting_img} source={{
-          uri: 'https://i.ibb.co/QpNhm72/setting-profile.png'
-        }}/>
-         <Text style={styles.setting_user_email}>Email: {auth.currentUser?.email}</Text>
-        
+    <ScrollView >
+      <View>
+        {/* div brefore first horizental break */}
+        <View style={styles.container}>
+          <Image style={styles.setting_img} source={{
+            uri: 'https://i.ibb.co/QpNhm72/setting-profile.png'
+          }} />
+          <Text style={styles.setting_user_email}>Email: {auth.currentUser?.email}</Text>
+
         </View>
         {/* div brefore first horizental break end*/}
 
-        <Divider style={{ backgroundColor: 'black',height:2,marginTop:30 }} />
+        <Divider style={{ backgroundColor: 'black', height: 2, marginTop: 30 }} />
 
         {/* div brefore second horizental break*/}
-        <View  style={{ marginLeft:10}}>
-          
-          <TouchableOpacity onPress={() =>navigation.navigate("Home")}>
-          <View style={styles.row}>
-          <Image style={styles.icon} source={{
-          uri: 'https://i.ibb.co/3W5D6Qy/home.png'
-        }} />
-          <Text style={styles.setting_Text}>Home</Text>
-          </View>
-          </TouchableOpacity>
-          
-          
-          <TouchableOpacity onPress={() =>navigation.navigate("RentData")}>
-          <View style={styles.row}>
-          <Image style={styles.icon} source={{
-          uri: 'https://i.ibb.co/zSJ7QQz/rent.png'
-        }} />
-          <Text style={styles.setting_Text}>Home Rent</Text>
-          </View>
+        <View style={{ marginLeft: 10 }}>
+
+          <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+            <View style={styles.row}>
+              <Image style={styles.icon} source={{
+                uri: 'https://i.ibb.co/3W5D6Qy/home.png'
+              }} />
+              <Text style={styles.setting_Text}>Home</Text>
+            </View>
           </TouchableOpacity>
 
 
-          <TouchableOpacity onPress={() =>navigation.navigate("PostForRent")}>
-         <View style={styles.row}>
-          <Image style={styles.icon} source={{
-          uri: 'https://i.ibb.co/zSJ7QQz/rent.png'
-        }} />
-          <Text style={styles.setting_Text}>Post your Home</Text>
-          </View>
+          <TouchableOpacity onPress={() => navigation.navigate("RentData")}>
+            <View style={styles.row}>
+              <Image style={styles.icon} source={{
+                uri: 'https://i.ibb.co/zSJ7QQz/rent.png'
+              }} />
+              <Text style={styles.setting_Text}>Home Rent</Text>
+            </View>
+          </TouchableOpacity>
+
+
+          <TouchableOpacity onPress={() => navigation.navigate("PostForRent")}>
+            <View style={styles.row}>
+              <Image style={styles.icon} source={{
+                uri: 'https://i.ibb.co/zSJ7QQz/rent.png'
+              }} />
+              <Text style={styles.setting_Text}>Post your Home</Text>
+            </View>
           </TouchableOpacity>
 
           <View style={styles.row}>
-          <Image style={styles.icon} source={{
-          uri: 'https://i.ibb.co/x3p9cSj/count.png'
-        }} />
-          <Text style={styles.setting_Text}>Total Meal</Text>
+            <Image style={styles.icon} source={{
+              uri: 'https://i.ibb.co/x3p9cSj/count.png'
+            }} />
+            <Text style={styles.setting_Text}>Total Meal</Text>
           </View>
 
           <View style={styles.row}>
-          <Image style={styles.icon} source={{
-          uri: 'https://i.ibb.co/QpNhm72/setting-profile.png'
-        }} />
-          <Text style={styles.setting_Text}>Add Meal</Text>
+            <Image style={styles.icon} source={{
+              uri: 'https://i.ibb.co/QpNhm72/setting-profile.png'
+            }} />
+            <Text style={styles.setting_Text}>Add Meal</Text>
           </View>
 
-         {
-           Admin?
-           <TouchableOpacity onPress={() =>navigation.navigate("AddMember")}>
-           <View style={styles.row}>
-           <Image style={styles.icon} source={{
-           uri: 'https://i.ibb.co/4FGZBmp/3631618.png'
-         }}/>
-           <Text style={styles.setting_Text}>Add Member</Text>
-           </View></TouchableOpacity>:<Text></Text>
-         }
-         
+          {
+            Admin ?
+              <TouchableOpacity onPress={() => navigation.navigate("AddMember")}>
+                <View style={styles.row}>
+                  <Image style={styles.icon} source={{
+                    uri: 'https://i.ibb.co/4FGZBmp/3631618.png'
+                  }} />
+                  <Text style={styles.setting_Text}>Add Member</Text>
+                </View></TouchableOpacity> : <Text></Text>
+          }
+
 
           {/* div brefore second horizental break end*/}
           {/* mess All member */}
-          <TouchableOpacity onPress={() =>navigation.navigate("MessMember")}>
-           <View style={styles.row}>
-           <Image style={styles.icon} source={{
-           uri: 'https://i.ibb.co/4FGZBmp/3631618.png'
-         }}/>
-           <Text style={styles.setting_Text}>All Member</Text>
-           </View></TouchableOpacity>
-          <Divider style={{ backgroundColor: 'black',height:2,marginTop:30 }} />
-          
+          <TouchableOpacity onPress={() => navigation.navigate("MessMember")}>
+            <View style={styles.row}>
+              <Image style={styles.icon} source={{
+                uri: 'https://i.ibb.co/4FGZBmp/3631618.png'
+              }} />
+              <Text style={styles.setting_Text}>All Member</Text>
+            </View></TouchableOpacity>
+          <Divider style={{ backgroundColor: 'black', height: 2, marginTop: 30 }} />
+
           {/* after second horizental*/}
-         <TouchableOpacity onPress={handleSignOut}>
-         <View style={styles.row}>
-          <Image style={styles.icon} source={{
-          uri: 'https://i.ibb.co/CMqDBcR/logout.png'
-        }}/>
-          <Text style={styles.setting_Text}>Logout</Text>
-         </View>
-         </TouchableOpacity>
+          <TouchableOpacity onPress={handleSignOut}>
+            <View style={styles.row}>
+              <Image style={styles.icon} source={{
+                uri: 'https://i.ibb.co/CMqDBcR/logout.png'
+              }} />
+              <Text style={styles.setting_Text}>Logout</Text>
+            </View>
+          </TouchableOpacity>
           {/* after second horizental end */}
         </View>
-        
 
-      
-  
-       
-  
-    </View>
+
+
+
+
+
+      </View>
+    </ScrollView>
   )
 }
 
@@ -164,37 +164,37 @@ const styles = StyleSheet.create({
   setting_img:
   {
 
-    marginStart:20,
-    marginTop:20,
-    width:70,
-    height:70
+    marginStart: 20,
+    marginTop: 20,
+    width: 70,
+    height: 70
   },
   setting_user_email:
   {
-    
-    marginStart:30,
-    marginTop:40,
-    color:'black',
-    fontWeight:'bold',
+
+    marginStart: 30,
+    marginTop: 40,
+    color: 'black',
+    fontWeight: 'bold',
     fontSize: 15,
   },
-  setting_Text:{
-    color:'black',
+  setting_Text: {
+    color: 'black',
     fontSize: 17,
-    marginTop:35,
-    marginStart:10
+    marginTop: 35,
+    marginStart: 10
 
   },
   icon:
   {
-    marginTop:30,
-    marginStart:5,
-    width:30,
-    height:30
+    marginTop: 30,
+    marginStart: 5,
+    width: 30,
+    height: 30
   },
-  row:{
+  row: {
     flexDirection: "row",
     flexWrap: "wrap"
   }
-    
+
 })
