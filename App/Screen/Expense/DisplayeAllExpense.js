9c57e2d31535b22c30cf59428fe2f179f755a335
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-03-11 23:55:37 +0600

    total expenses fixed

--- App/Screen/Expense/DisplayeAllExpense.js
+++ App/Screen/Expense/DisplayeAllExpense.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native'
 import React from 'react'
 import { Divider } from 'react-native-elements'
 
@@ -6,65 +6,67 @@ const DisplayeAllExpense = (props) => {
   const { month, date, year, expense, cost } = props.data
 
   return (
-    <View>
-      <View style={styles.container}>
+    <ScrollView>
+      <View>
+        <View style={styles.container}>
 
-        <View style={styles.inputView}>
-          <TextInput
-            style={styles.TextInput}
-            multiline={true}
-            numberOfLines={4}
-            value={month}
-            placeholderTextColor="#003f5c"
-            keyboardType="email-address"
-          />
-        </View>
-        <View style={styles.inputView1}>
-          <TextInput
-            style={styles.TextInput}
-            multiline={true}
-            numberOfLines={4}
-            value={date}
-            placeholderTextColor="#003f5c"
-            keyboardType="email-address"
-          />
-        </View>
-        <View style={styles.inputView1}>
-          <TextInput
-            style={styles.TextInput}
-            multiline={true}
-            numberOfLines={4}
-            value={year}
-            placeholderTextColor="#003f5c"
-            keyboardType="email-address"
-          />
-        </View>
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              multiline={true}
+              numberOfLines={4}
+              value={month}
+              placeholderTextColor="#003f5c"
+              keyboardType="email-address"
+            />
+          </View>
+          <View style={styles.inputView1}>
+            <TextInput
+              style={styles.TextInput}
+              multiline={true}
+              numberOfLines={4}
+              value={date}
+              placeholderTextColor="#003f5c"
+              keyboardType="email-address"
+            />
+          </View>
+          <View style={styles.inputView1}>
+            <TextInput
+              style={styles.TextInput}
+              multiline={true}
+              numberOfLines={4}
+              value={year}
+              placeholderTextColor="#003f5c"
+              keyboardType="email-address"
+            />
+          </View>
 
-      </View>
-      <View style={styles.container}>
-        <View style={styles.inputView2}>
-          <TextInput
-            style={styles.TextInput}
-            multiline={true}
-            numberOfLines={4}
-            value={expense}
-            placeholderTextColor="#003f5c"
-            keyboardType="email-address"
-          />
         </View>
-        <View style={styles.inputView3}>
-          <TextInput
-            style={styles.TextInput}
-            multiline={true}
-            numberOfLines={4}
-            value={cost}
-            placeholderTextColor="#003f5c"
-            keyboardType="email-address"
-          />
+        <View style={styles.container}>
+          <View style={styles.inputView2}>
+            <TextInput
+              style={styles.TextInput}
+              multiline={true}
+              numberOfLines={4}
+              value={expense}
+              placeholderTextColor="#003f5c"
+              keyboardType="email-address"
+            />
+          </View>
+          <View style={styles.inputView3}>
+            <TextInput
+              style={styles.TextInput}
+              multiline={true}
+              numberOfLines={4}
+              value={cost}
+              placeholderTextColor="#003f5c"
+              keyboardType="email-address"
+            />
+          </View>
         </View>
+        <Divider style={{ backgroundColor: '#37342B', height: 2, marginTop: 7 }} />
       </View>
-      <Divider style={{ backgroundColor: '#37342B', height: 2, marginTop: 7 }} />
-    </View>
+    </ScrollView>
   )
 }
 
@@ -73,7 +75,7 @@ export default DisplayeAllExpense
 const styles = StyleSheet.create({
   container:
   {
-
+    paddingBottom: 25,
     flexDirection: 'row',
 
   },
